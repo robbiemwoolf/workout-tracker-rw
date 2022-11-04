bf8a419fcfc53e5f55bc3da5c3ad386a540363d9
Robbie Woolf <robbiemwoolf@gmail.com>
2022-11-03 20:55:57 -0700

    added temp bg and logo; created Push component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css'
+import React from 'react'
+import Push from './push/Push'
+
 
 function App() {
   return (
-    <div>
+    <div className='App'>
+        <Push />
     </div>
-  );
+  )
 }
 
 export default App
---
+++ src/push/Push.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Push.css'
+
+export default function Push() {
+    return (
+        <div class="card">
+            <h3>Push Day</h3>
+        </div>
+    )
+}
