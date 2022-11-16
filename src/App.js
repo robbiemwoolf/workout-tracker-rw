6c630846d46db2a0c5a8c3f3c3446f896a1c177f
Robbie Woolf <robbiemwoolf@gmail.com>
2022-11-16 10:23:28 -0800

    better organization of code

--- src/App.js
+++ src/App.js
@@ -1,34 +1,26 @@
 import React from 'react'
 import { Routes, Route, Link } from 'react-router-dom'
 import './App.css'
-import Push from './push/Push'
-import Pull from './pull/Pull'
-import Leg from './leg/Leg'
-import Upper from './upper/Upper'
-import Lower from './lower/Lower'
+import Push from './pages/push/Push'
+import Pull from './pages/pull/Pull'
+import Leg from './pages/leg/Leg'
+import Upper from './pages/upper/Upper'
+import Lower from './pages/lower/Lower'
+import NotFound from './pages/NotFound'
+import Home from './pages/Home'
+import Plan from './dayLayout/Plan'
 
 function App() {
-    const days = ['Push', 'Pull', 'Leg', 'Upper', 'Lower']
-    const card = days.map((day) => {
-        return (
-                    <Link to={day} style={{textDecoration: 'none'}}>
-                        <div key={day} className='App'>
-                            <div className='homeCard'>
-                                <h3 className='greenText glowGreen'>{day} Day</h3>
-                            </div>
-                        </div>
-                    </Link>
-        )
-    })
-
   return (
     <Routes>
-        <Route path='/' element={card} />
+        <Route path='/' element={<Home />} />
         <Route path='/push' element={<Push />} />
+        <Route path='/push/plan' element={<Plan />} />
         <Route path='/pull' element={<Pull />} />
         <Route path='/leg' element={<Leg />} />
         <Route path='/upper' element={<Upper />} />
         <Route path='/lower' element={<Lower />} />
+        <Route path='*' element={<NotFound />} />
     </Routes>
   )
 }
