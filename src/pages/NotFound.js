228b8d483212b5d381702e120fc8dfd715633cc3
Robbie Woolf <robbiemwoolf@gmail.com>
2022-11-16 10:20:09 -0800

    created a 404 page

---
+++ src/pages/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function NotFound() {
+    return ( 
+        <> 
+           <div className='container dark'>
+                <h1 className='neonText glow'>404 Error: Page Not Found</h1>
+            </div>          
+        </>
+    )
+}
