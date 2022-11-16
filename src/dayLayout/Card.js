f09007602ba7a3337bb03ff5ea047918871f4428
Robbie Woolf <robbiemwoolf@gmail.com>
2022-11-16 10:22:56 -0800

    harded coded for push path for now

--- src/dayLayout/Card.js
+++ src/dayLayout/Card.js
@@ -7,7 +7,7 @@ export default function Card() {
             <div className='card'>
                 <p className='query neonText glow'>Are you ready?</p>
                 <button className='btn neonText glow'>Let's go!</button>
-                <Link to='plan' style={{textDecoration: 'none', textAlign: 'center' }}>
+                <Link to='/push/plan' style={{textDecoration: 'none', textAlign: 'center' }}>
                     <small className='plan pinkText'>Preview the plan</small>
                 </Link>
             </div>
