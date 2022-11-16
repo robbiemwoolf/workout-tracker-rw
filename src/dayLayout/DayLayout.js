e6d7e9bfb18e04d045c243cefa6f17146f3917cf
Robbie Woolf <robbiemwoolf@gmail.com>
2022-11-16 10:22:14 -0800

    DayLayout for future utilization

---
+++ src/dayLayout/DayLayout.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../pages/push/Push.css'
+import Header from './Header'
+import Card from './Card'
+
+export default function DayLayout() {
+    return (
+        <>
+            <Header />
+            <Card />
+        </>
+    )
+}
