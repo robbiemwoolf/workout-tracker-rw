f362eb22e64e8f4f467cbfe1aaae707af7c21b63
Robbie Woolf <robbiemwoolf@gmail.com>
2022-11-16 18:10:14 -0800

    updated component to be used across multiple pages

--- src/dayLayout/Card.js
+++ src/dayLayout/Card.js
@@ -1,14 +1,15 @@
 import React from 'react'
+import '../pages/push/Push.css'
 import { Link } from 'react-router-dom'
 
-export default function Card() {
+export default function Card({path}) {
     return (        
         <div className='container'>
             <div className='card'>
                 <p className='query neonText glow'>Are you ready?</p>
                 <button className='btn neonText glow'>Let's go!</button>
-                <Link to='/push/plan' style={{textDecoration: 'none', textAlign: 'center' }}>
-                    <small className='plan pinkText'>Preview the plan</small>
+                <Link to={path} className='preview pinkText'>
+                    Preview the plan
                 </Link>
             </div>
         </div>
