fc7a1f246ab32b4281f2a8e0d63e44ff6e5928d3
Robbie Woolf <robbiemwoolf@gmail.com>
2022-11-16 18:06:58 -0800

    added plan paths

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Routes, Route, Link } from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import './App.css'
 import Push from './pages/push/Push'
 import Pull from './pages/pull/Pull'
@@ -8,18 +8,26 @@ import Upper from './pages/upper/Upper'
 import Lower from './pages/lower/Lower'
 import NotFound from './pages/NotFound'
 import Home from './pages/Home'
-import Plan from './dayLayout/Plan'
+import PushPlan from './pages/push/PushPlan'
+import PullPlan from './pages/pull/PullPlan'
+import LegPlan from './pages/leg/LegPlan'
+import UpperPlan from './pages/upper/UpperPlan'
+import LowerPlan from './pages/lower/LowerPlan'
 
 function App() {
   return (
     <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/push' element={<Push />} />
-        <Route path='/push/plan' element={<Plan />} />
+        <Route path='/push/plan' element={<PushPlan />} />
         <Route path='/pull' element={<Pull />} />
+        <Route path='/pull/plan' element={<PullPlan />} />
         <Route path='/leg' element={<Leg />} />
+        <Route path='/leg/plan' element={<LegPlan />} />
         <Route path='/upper' element={<Upper />} />
+        <Route path='/upper/plan' element={<UpperPlan />} />
         <Route path='/lower' element={<Lower />} />
+        <Route path='/lower/plan' element={<LowerPlan />} />
         <Route path='*' element={<NotFound />} />
     </Routes>
   )
