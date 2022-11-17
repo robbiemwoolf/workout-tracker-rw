ed7ea00b84f886441de9a51b222be0f46083ee48
Robbie Woolf <robbiemwoolf@gmail.com>
2022-11-16 18:10:51 -0800

    moved key to appropriate child component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,8 +5,8 @@ export default function Home() {
     const days = ['Push', 'Pull', 'Leg', 'Upper', 'Lower']
     const card = days.map((day) => {
         return (
-                    <Link to={day} style={{textDecoration: 'none'}}>
-                        <div key={day} className='App'>
+                    <Link to={day} style={{textDecoration: 'none'}} key={day}>
+                        <div className='App'>
                             <div className='homeCard'>
                                 <h3 className='greenText glowGreen'>{day} Day</h3>
                             </div>
